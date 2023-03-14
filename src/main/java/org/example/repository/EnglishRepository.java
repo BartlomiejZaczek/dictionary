d6a42e913054bab58a5ebef1848807b85d1f1ae4
BartlomiejZaczek <bartlomiej.zaczek@marotino.ventures>
2023-03-14 11:20:33 +0100

    delete redundant files

--- src/main/java/org/example/repository/EnglishRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.repository;
-
-import org.example.repository.entity.English;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnglishRepository extends JpaRepository<English, Long> {
-
-}
